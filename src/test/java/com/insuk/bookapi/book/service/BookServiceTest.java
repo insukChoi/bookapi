2538efa2d7871576ddc343296f40e05fdfe150f6
insuk <cis1725@gmail.com>
2019-09-05 22:50:51 +0900

    test 수정

--- src/test/java/com/insuk/bookapi/book/service/BookServiceTest.java
+++ src/test/java/com/insuk/bookapi/book/service/BookServiceTest.java
@@ -1,10 +1,9 @@
 package com.insuk.bookapi.book.service;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.insuk.bookapi.book.dto.kakao.BookDto;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,13 +15,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @JsonTest
-@Ignore
 public class BookServiceTest {
 
+    @Autowired
     private JacksonTester<BookDto> json;
 
     @Test
-    public void 도서검색API_json_To_Dto() throws IOException, JsonParseException {
+    public void 도서검색API_json_To_Dto() throws IOException {
         String bookApicontent = "{\"title\":\"모략의 즐거움\",\"thumbnail\":\"https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F518915%3Ftimestamp%3D20190123153425\",\"contents\":\"\",\"isbn\":\"8934925094 9788934925095\",\"authors\":[\"마수취안\"],\"publisher\":\"김영사\",\"datetime\":\"2007-04-22T15:00:00.000Z\",\"price\":14000,\"sale_price\":-1}";
 
         BookDto bookDto = new BookDto();
@@ -32,7 +31,7 @@ public class BookServiceTest {
         assertThat(this.json.parseObject(bookApicontent).getTitle())
                 .isEqualTo(bookDto.getTitle());
         assertThat(this.json.parseObject(bookApicontent).getThumbnail()).isEqualTo(bookDto.getThumbnail());
-        assertThat(this.json.parseObject(bookApicontent).getIsbn()).isNull();
+
     }
 
     /*@Test
